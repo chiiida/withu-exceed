5ab16718ae5832052e4065edb6bfb9b6d2d7c593
Phitchawat Lukkanathiti <me@shayennn.com>
2019-07-13 02:07:09 +0700

    Random example value when no one at sensor

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,9 @@
 from flask import Flask, escape, request, jsonify
 import requests
+import json
+import time
+import random
+
 app = Flask(__name__)
 
 
@@ -15,22 +19,44 @@ def data():
         'https://exceed.superposition.pknn.dev/data/withu').json()
     data = request.json
     newdat = {
-        'msg': '',
-        'bpm': [],
-        'vibration': []
+        'data': {
+            'msg': '',
+            'bpm': [],
+            'vibration': [],
+            'alert': False,
+            'timestamp': []
+        }
     }
     if data is not None:
-        if 'msg' in old_dat and old_dat['msg'] is not None:
-            newdat['msg'] = old_dat['msg']
-        if 'bpm' in old_dat and old_dat['bpm'] is not None:
-            newdat['bpm'] = old_dat['bpm']
-        if 'vibration' in old_dat and old_dat['vibration'] is not None:
-            newdat['vibration'] = old_dat['vibration']
-        newdat['bpm'].append(float(data['data']['bpm']))
-        newdat['vibration'].append(int(data['data']['vibration']))
-        requests.post(
-            'https://exceed.superposition.pknn.dev/data/withu', json=newdat)
+        if data['data']['bpm'] < 40:
+            data = {
+                'data': {
+                    'bpm': random.randint(50, 70),
+                    'vibration':  int(1/random.randint(1, 200)*50)
+                }
+            }
+        if old_dat is not None and 'alert' in old_dat and old_dat['alert'] is not None:
+            newdat['data']['alert'] = old_dat['alert']
+        if old_dat is not None and 'timestamp' in old_dat and old_dat['timestamp'] is not None:
+            newdat['data']['timestamp'] = old_dat['timestamp']
+        if old_dat is not None and 'msg' in old_dat and old_dat['msg'] is not None:
+            newdat['data']['msg'] = old_dat['msg']
+        if old_dat is not None and 'bpm' in old_dat and old_dat['bpm'] is not None:
+            newdat['data']['bpm'] = old_dat['bpm']
+        if old_dat is not None and 'vibration' in old_dat and old_dat['vibration'] is not None:
+            newdat['data']['vibration'] = old_dat['vibration']
+        newdat['data']['bpm'].append(float(data['data']['bpm']))
+        newdat['data']['vibration'].append(int(data['data']['vibration']))
+        newdat['data']['timestamp'].append(time.strftime("%Y/%m/%d %H:%M:%S"))
+        while len(newdat['data']['bpm']) > 50:
+            del newdat['data']['bpm'][0]
+            del newdat['data']['vibration'][0]
+            del newdat['data']['timestamp'][0]
+        headers = {'Content-type': 'application/json'}
+        save = requests.post(
+            'https://exceed.superposition.pknn.dev/data/withu',
+            data=json.dumps(newdat), headers=headers)
     return jsonify({
-        'status': True,
-        'msg': 'OK'
+        'status': 'SUCCESS',
+        'message': 'OK'
     })
