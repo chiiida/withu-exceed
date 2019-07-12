cc41d86b8a5bc6054881529f5fbbb1f0601725fe
Nontosan <non.vit@ku.th>
2019-07-12 15:20:27 +0700

    vibrator + post data

--- hardware/main.py
+++ hardware/main.py
@@ -5,13 +5,15 @@ import urequests as requests
 from _thread import start_new_thread as thread
 import network
 
-p_hit = Pin(27,Pin.IN)
+p_vibration = Pin(27,Pin.IN)
 p_RLED = Pin(14, Pin.OUT) 
 p_GLED = Pin(12, Pin.OUT) 
 p_BLED = Pin(13, Pin.OUT)
 
 WIFISTATUS = False
 LEDSTATUS = 'disconnected'
+API = "https://exceed.superposition.pknn.dev/data/withu"
+VIBRATIONSTATUS = False
 
 def WIFIConnect():
   global WIFISTATUS, LEDSTATUS
@@ -26,16 +28,28 @@ def WIFIConnect():
   LEDSTATUS = 'connected'
 
 def WIFICheck():
+  global LEDSTATUS
   while True:
     if not WIFISTATUS:
       LEDSTATUS = 'disconnected'
       WIFIConnect()
     sleep(2)
   
-def HitSensor():
+def vibrationSensor():
+  global VIBRATIONSTATUS,vibration
   while(True):
-    print(p_hit.value())
-    sleep(0.000001)
+    count = 0
+    for i in range (60000):
+      if (p_vibration == 0):
+        count += 1
+      sleep(0.001)
+    if (count >= 5000):
+      vibration = True
+    else:
+      vibration = False
+    VIBRATIONSTATUS = True
+    sleep(0.01)
+  
 
 def statusLED():
   while(True):
@@ -57,6 +71,28 @@ def statusLED():
       p_BLED.value(0)
   sleep(0.01)
 
+def postData():
+    global API, vibration
+    while True:
+        if (vibration):
+          print('Vibration = True')
+        else:
+          print('Vibration = False')          
+        if WIFISTATUS:
+          r = requests.get(API)
+          json_data = r.json()
+          print(data)
+          data = json.dumps({
+            'vibration': vibration
+          })
+          headers = {'Content-type': 'application/json'}
+          print(requests.post(API, data=data, headers=headers).content)
+          VIBRATIONSTATUS = False
+      sleep(0.01)
+
+
 WIFIConnect()
-thread(WIFICheck, [])
-thread(statusLED(), [])
+#thread(WIFICheck, [])
+#thread(statusLED(), [])
+thread(vibrationSensor(), [])
+thread(postData(), [])
