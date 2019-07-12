9d0592fb55e5ad024fa9f80a4faa3dd4e36fedab
Nontosan <non.vit@ku.th>
2019-07-12 13:56:48 +0700

    led status

--- hardware/testHR.py
+++ hardware/testHR.py
@@ -2,7 +2,20 @@ from machine import Pin, ADC
 from time import sleep
 
 hr = ADC(Pin(34))
+led = Pin(5,Pin.OUT)
+ 
+alpha = 0.75
+period = 0.02
+change = 0.0
 
-while(True):
-  print(hr.read())
-  sleep(0.01)
+while (True):
+  oldValue = 0
+  oldChange = 0
+  rawValue = hr.read()
+  value = alpha * oldValue + (1-alpha) * rawValue
+  change = value - oldValue
+  if (change < 0.0 and oldChange > 0.0):
+    led.value(1)
+  oldValue = value
+  oldChange = change
+  delay(period)
--- hardware/testHit.py
+++ hardware/testHit.py
@@ -1,8 +1,9 @@
 from machine import Pin, ADC
 from time import sleep
 
-hit = Pin(27,Pin.IN)
+p_hit = Pin(27,Pin.IN)
 
-while(True):
-  print(hit.value())
-  sleep(0.01)
+def HitSensor():
+  while(True):
+    print(p_hit.value())
+    sleep(0.000001)
