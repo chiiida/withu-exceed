12c2d39eedd6e7a32066a3968427ea52df454a67
Peepers <eyepeepers@hotmail.com>
2019-07-12 15:00:00 +0700

    js

---
+++ js/doctor.js
@@ -0,0 +1,28 @@
+const baseURL = 'https://exceed.superposition.pknn.dev';
+
+let mainData = {
+	"data": {
+        "msg": "",
+        "bpm" : []
+	}
+}
+
+function getDocMsg() {
+    mainData.data.msg = $('#docMsg').val()
+
+    return mainData
+}
+
+// POST
+function postMsg() {
+    fetch(baseURL + '/data/withu', {
+        method: 'POST',
+        body: JSON.stringify(getDocMsg()),
+        headers:{
+          'Content-Type': 'application/json'
+        }
+      }).then(res => res.json())
+      .then(response => console.log('Success:', JSON.stringify(response)))
+      .catch(error => console.error('Error:', error));
+}
+
---
+++ js/quote-data.js
@@ -0,0 +1,10 @@
+let quote = {
+    normal: ["Every man has his secret sorrows which the world knows not; and often times we call a man cold when he is only sad.” ― Henry Wadsworth Longfellow",
+            "I didn’t want to wake up. I was having a much better time asleep. And that’s really sad. It was almost like a reverse nightmare, like when you wake up from a nightmare you’re so relieved. I woke up into a nightmare.” – Ned Vizzini, It’s Kind of a Funny Story",
+            "There are wounds that never show on the body that are deeper and more hurtful than anything that bleeds.” ― Laurell K. Hamilton, Mistral’s Kiss",
+            ],
+    danger: ["Whenever you read a cancer booklet or website or whatever, they always list depression among the side effects of cancer. But, in fact, depression is not a side effect of cancer. Depression is a side effect of dying.” – John Green, The Fault in Our Stars",
+            "That’s the thing about depression: A human being can survive almost anything, as long as she sees the end in sight. But depression is so insidious, and it compounds daily, that it’s impossible to ever see the end.” – Elizabeth Wurtzel, Prozac Nation",
+            ],
+            
+}
