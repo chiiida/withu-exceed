66d5dee0390a2acecbe4af27d7c1c50454ad0320
Peepers <eyepeepers@hotmail.com>
2019-07-13 03:57:20 +0700

    js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 function doctorSure() {
     var password = prompt("Please enter doctor ID");
-    if (password == '12345678') {
+    if (password == '1') {
         document.getElementById('docPage').href = "doctor.html";
     } else {
         document.getElementById('docPage').href = "#";
