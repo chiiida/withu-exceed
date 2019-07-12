dfc1a7ce0a3f324b54d7298be604fef905b0eebb
Nontosan <non.vit@ku.th>
2019-07-12 11:07:29 +0700

    WIFI Fix

--- hardware/main.py
+++ hardware/main.py
@@ -5,24 +5,24 @@ import urequests as requests
 from _thread import start_new_thread as thread
 import network
 
+WIFISTATUS = False
 
 def WIFIConnect():
+  global WIFISTATUS
   wlan = network.WLAN(network.STA_IF)
   wlan.active(True)
-  while True:
-    WIFISTATUS = 0
+  WIFISTATUS = False
+  wlan.connect('exceed16_8', '12345678')
+  while not wlan.isconnected():
     print('connecting...')
-    wlan.connect('exceed16_8', '12345678')
-    while not wlan.isconnected():
-      sleep(0.01)
-      WIFISTATUS = 1
-      print('connected')
-    while wlan.isconnected():
-      sleep(2)
+    sleep(0.01)
+  WIFISTATUS = wlan.isconnected()
+  print('connected')
+
 
 def WIFICheck():
   while True:
-    if not wlan.isconnected():
+    if not WIFISTATUS:
       WIFIConnect()
     sleep(2)
   
