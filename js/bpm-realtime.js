8b2ff30e216ce42d7677aeb52eed9adb2142e464
Peepers <eyepeepers@hotmail.com>
2019-07-13 11:00:53 +0700

    2nd fix

--- js/bpm-realtime.js
+++ js/bpm-realtime.js
@@ -1,7 +1,21 @@
-const myURL = 'https://exceed.superposition.pknn.dev';
+const mainURL = 'https://exceed.superposition.pknn.dev';
+
+function putAlert(status) {
+  fetch(mainURL + '/data/withu/alert', {
+      method: 'PUT',
+      body: JSON.stringify({
+          "value" : status
+      }),
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+    .then(response => console.log('Success:', JSON.stringify(response)))
+    .catch(error => console.error('Error:', error));
+}
 
 function getBpm() {
-    fetch(myURL + '/data/withu/bpm')
+    fetch(mainURL + '/data/withu/bpm')
   .then(function(response) {
     return response.json();
   })
@@ -10,6 +24,32 @@ function getBpm() {
   });
 }
 
+function alertProb() {
+  fetch(mainURL + '/data/withu/bpm')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    var bpm = myJson;
+
+    fetch(mainURL + '/data/withu/vibration')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      var vibration = myJson;
+
+      for (var i = 0; i < vibration.lenght; i++) {
+        if (bpm[i] > 100 && vibration[i] < 30) {
+          putAlert(true)
+          setTimeout(() => putAlert(false), 10000);
+        }
+      }
+    });
+  });
+}
+
 setInterval(() => {
   getBpm();
+  alertProb();
 }, 1000)
--- js/timer.js
+++ js/timer.js
@@ -1,5 +1,5 @@
 let quoteDay = {
-    normal: ["Every man has his secret sorrows which the world knows not; and often times we call a man cold when he is only sad.” ― Henry Wadsworth Longfellow",
+    normal: [
             "I didn’t want to wake up. I was having a much better time asleep. And that’s really sad. It was almost like a reverse nightmare, like when you wake up from a nightmare you’re so relieved. I woke up into a nightmare.” – Ned Vizzini, It’s Kind of a Funny Story",
             "There are wounds that never show on the body that are deeper and more hurtful than anything that bleeds.” ― Laurell K. Hamilton, Mistral’s Kiss",
             "Whenever you read a cancer booklet or website or whatever, they always list depression among the side effects of cancer. But, in fact, depression is not a side effect of cancer. Depression is a side effect of dying.” – John Green, The Fault in Our Stars",
